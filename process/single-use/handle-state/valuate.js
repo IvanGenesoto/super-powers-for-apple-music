7ce8bb047ed423bf48e1256fa82c0f1c0cce040a
Ivan Genesoto <ivan@Ivans-MBP.lan1>
2019-06-23 18:03:22 -0700

    Multiple validation sets are accommodated.

--- process/single-use/handle-state/valuate.js
+++ process/single-use/handle-state/valuate.js
@@ -18,21 +18,21 @@ module.exports = function valuate(valuation, [label, kit]) {
       getDefaultFieldValue
     } = kit
     const key = isField ? field : label
-    const wrappedPreValue = isField ? wrappedFieldPreValue : wrappedLabelPreValue
+    const wrappedValue = isField ? wrappedFieldPreValue : wrappedLabelPreValue
     const valueByKey = isField ? valueByField : valueByLabel
     const defaultValue = isField ? defaultFieldValue : defaultLabelValue
     const getDefaultValue = isField ? getDefaultFieldValue : getDefaultLabelValue
     const getValue = isField ? getFieldValue : getTagValue
     const didSetByArtist = state[antiAdoptionStateKey]
     if (didSetByArtist && didSetByArtist[artist]) return
-    artistTracks.find(appendPreValue, {key, getValue, wrappedPreValue})
-    const {preValue} = wrappedPreValue
-    const value =
-      preValue ||
+    artistTracks.find(appendPreValue, {key, getValue, wrappedValue})
+    const {value} = wrappedValue
+    const value_ =
+      value ||
       defaultValue ||
       (getDefaultValue && getDefaultValue(artistTracks[0])) ||
-      (isField && columnBrowserFields.includes(field) && '-')
-    value && (valueByKey[key] = value)
+      (isField && columnBrowserFields.includes(field) && '-') // #note: Deprecated with iTunes.
+    value_ && (valueByKey[key] = value_)
   }
 
   const getFieldValue = function(key) {
@@ -41,10 +41,10 @@ module.exports = function valuate(valuation, [label, kit]) {
   }
 
   const appendPreValue = function(track) {
-    const {key, getValue, wrappedPreValue} = this
+    const {key, getValue, wrappedValue} = this
     const data = track.properties()
     const value = getValue.call({data}, key)
-    return (wrappedPreValue.preValue = value)
+    return (wrappedValue.value = value)
   }
 
   appendValue(label, kit)
