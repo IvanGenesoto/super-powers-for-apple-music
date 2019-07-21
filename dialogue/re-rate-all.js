300cc9a3adb5e3d89c16ac2d7371d09cbbfa5d13
Ivan Genesoto <ivan@Ivans-MBP.lan1>
2019-07-21 16:15:11 -0700

    Updated setUnplayed.

--- dialogue/re-rate-all.js
+++ dialogue/re-rate-all.js
@@ -1,17 +1,14 @@
-module.exports = () => {
+module.exports = () => { // #note: Unused in favor of setting in "Preferences" playlist's description.
 
   const app = require('../app')
   const buttons = ['Cancel', 'Yes', 'No']
 
-  const {buttonReturned} = app.displayDialog(
-    'Re-rate all artists?',
-    {
-      buttons,
-      cancelButton: 'Cancel',
-      defaultButton: 'No',
-      withIcon: 1
-    }
-  )
+  const {buttonReturned} = app.displayDialog('Re-rate all artists?', {
+    buttons,
+    cancelButton: 'Cancel',
+    defaultButton: 'No',
+    withIcon: 1
+  })
 
   return buttonReturned === 'Yes'
 }
--- process/index.js
+++ process/index.js
@@ -2,15 +2,24 @@ module.exports = function process(isTest) {
 
   const createState = require('./single-use/create-state')
   const getPlaylist = require('../get/playlist')
+  // const getShouldProcessPlayed = require('../get/should-process-played') // #implement: User can set if they want to process all played or changed tracks via the "Preferences" playlist's description.
+  // const processPlayed = require('./played') // #implement: Adds an artist-named property to shouldDeriveRatingByArtist for all tracks whose "unplayed" is false.
   const handleSmartPlaylists = require('./single-use/handle-smart-playlists')
   const executeAndRecurse = require('./multi-use/execute-and-recurse')
   const handleState = require('./single-use/handle-state')
-  const setUnplayed = require('./single-use/set-unplayed')
-  const commandsFolder = getPlaylist('1 Commands') // #feature: User can set name of commands folder in "Preferences" playlist.
+  // const setUnplayed = require('./single-use/set-unplayed')
+  const commandsFolder = getPlaylist('1 Commands') // #implement: User can set name of commands folder in "Preferences" playlist's description.
   const state = createState(isTest)
   const this_ = {state}
+  // const shouldProcessPlayed = getShouldProcessPlayed()
 
   handleSmartPlaylists.call(this_)
+  // shouldProcessPlayed && processPlayed()
   executeAndRecurse.call(this_, commandsFolder)
   handleState.call(this_)
+
+  // if (!shouldProcessPlayed) return
+
+  // delay(60) /* global delay */
+  // setUnplayed()
 }
