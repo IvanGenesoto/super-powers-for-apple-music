3bb3e212b05dbbceab10605ee3324ed24a4f9845
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2019-07-21 16:15:11 -0700

    Updated setUnplayed.

--- process/single-use/set-unplayed.js
+++ process/single-use/set-unplayed.js
@@ -1,5 +1,12 @@
 module.exports = function setUnplayed(track) {
 
-  try { track.unplayed.set(true) } // #debug: Must be set last as setting any other field will reset this.
-  catch (unused) { }
+  const {state} = this
+  const {_allTracks} = state
+
+  const setUnplayedToTrue = track => {
+    try { track.unplayed.set(true) }
+    catch (unused) { }
+  }
+
+  _allTracks.whose({unplayed: false})().forEach(setUnplayedToTrue)
 }
